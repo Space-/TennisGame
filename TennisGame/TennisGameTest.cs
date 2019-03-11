655eda7e1c0026712b8301c4f70d0d40b8cc77a4
Bruce <gkm85663@gmail.com>
2019-03-12 01:09:06 +0800

    Create unit test - GetTennisResult_Player1_score_1_Player_score_0

--- TennisGame/TennisGameTest.cs
+++ TennisGame/TennisGameTest.cs
@@ -20,13 +20,37 @@ namespace TennisGame
             // assert
             Assert.AreEqual(expected, gameResult);
         }
+
+        [TestMethod]
+        public void GetTennisResult_Player1_score_1_Player_score_0()
+        {
+            var player1 = new TennisPlayer(1);
+            var player2 = new TennisPlayer(0);
+            var scoreboard = new GameScoreboard();
+            const string expected = "15 Love";
+
+            var gameResult = scoreboard.GetGameResult(player1, player2);
+
+            Assert.AreEqual(expected, gameResult);
+        }
     }
 
     public class GameScoreboard
     {
         public string GetGameResult(TennisPlayer player1, TennisPlayer player2)
         {
-            return player1.Score == 0 && player1.Score == player2.Score ? "Love All" : "";
+            if (player1.Score == 0 && player1.Score == player2.Score)
+            {
+                return "Love All";
+            }
+            else if (player1.Score == 1 && player2.Score == 0)
+            {
+                return "15 Love";
+            }
+            else
+            {
+                return "";
+            }
         }
     }
 
