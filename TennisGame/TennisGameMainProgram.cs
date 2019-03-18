a27b52f7f859b16ebd8b3cedc32f0d529e90100c
bruce.kao38 <bruce.kao38@TI03NB134>
2019-03-18 11:51:05 +0800

    Create a console application -  'TennisGame'

---
+++ TennisGame/TennisGameMainProgram.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace TennisGame
+{
+    class TennisGameMainProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+        }
+    }
+}
